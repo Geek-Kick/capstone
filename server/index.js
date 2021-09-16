30a3402fad59f4912ba10eeb18fae4c8bed4353f
SeungYong Choi <ppyooy336@naver.com>
2021-09-16 17:52:15 +0900

    fork test

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("우리 모두 화이팅..!")
-})
+app.get("/", (req, res) => {
+  res.send("우리 모두 화이팅...!");
+});
 
-app.get("/test", (req,res) => {
-    res.send("통신 성공")
-})
+app.get("/test", (req, res) => {
+  res.send("통신 성공");
+});
 
 const port = 5000;
 app.listen(port, () => console.log(`Server is listening on ${port}`));
