8ef2a80b30fd7c2df2bcdc027bcfd9a3a5eeaa03
Hwang Seunghwan <xx0hnhwang@gmail.com>
2021-10-06 20:32:49 +0900

    Edit postPost api

--- server/src/api/community/communityService.js
+++ server/src/api/community/communityService.js
@@ -7,17 +7,20 @@ exports.postPost = async (req_body, userId, imageUrl_body) => {
   const con = await pool.getConnection(async conn => conn);
   const postQuery = dao.postPostQuery;
   const imageQuery = dao.postImageQuery;
+  const getPostQuery = dao.getPostQuery;
   const { title, category, contents } = req_body;
   const { imageUrl } = imageUrl_body;
   try {
     await con.beginTransaction();
     const postRow = await con.query(postQuery, [userId, title, category, contents]);
     const getPostRow = await con.query(getPostQuery, [userId, title, category, contents]);
-    for (let i = 0; i < imageUrl_body.length; i++) {
-      const imageRow = await con.query(imageQuery, [getPostRow[0].id, imageUrl_body[i].imageUrl]);
+    if (imageUrl_body) {
+      for (let i = 0; i < imageUrl_body.length; i++) {
+        const imageRow = await con.query(imageQuery, [getPostRow[0].id, imageUrl_body[i].imageUrl]);
+      }
     }
     await con.commit();
-    return postRow[0].postRows;
+    return 1;
   } catch (e) {
     await con.rollback();
     console.log(`Service error \n ${e}`);
