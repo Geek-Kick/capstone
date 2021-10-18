e573659fda67a13e82ddbe3199620d8b00fc4279
SeungYong Choi <ppyooy336@naver.com>
2021-10-18 21:57:03 +0900

    Add photo save in cloud(AWS s3)

--- server/src/api/example/exampleRoute.js
+++ server/src/api/example/exampleRoute.js
@@ -1,6 +1,12 @@
 const router = require("express").Router();
 const controller = require("./exampleController");
+// image 처리
+const upload = require("../../../config/upload");
 
 router.post("/test", controller.test);
+
+router.post("/image", upload.single("img"), controller.imageTest);
+
+// router.get("/getImage", controller.getImage);
 
 module.exports = router;
