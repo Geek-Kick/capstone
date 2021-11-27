050dc693164509693d477b88c3cd0e916177d312
Hwang Seunghwan <xx0hnhwang@gmail.com>
2021-11-27 19:50:46 +0900

    Re-add lecture apis

--- server/src/api/lecture/lectureJoi.js
+++ server/src/api/lecture/lectureJoi.js
@@ -1,7 +1,15 @@
 const joi = require('joi');
 
 const lectureId = joi.number();
+const star = joi.number();
+const contents = joi.string();
 
 exports.myLectureJoi = joi.object({
   lectureId: lectureId.required(),
+});
+
+exports.postReviewJoi = joi.object({
+  lectureId: lectureId.required(),
+  star: star.required(),
+  contents: contents.required(),
 });
